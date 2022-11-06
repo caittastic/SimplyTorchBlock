ae8cbbc2cea0bc08ab215eef39f25cbcd3275f02
smoluwu <79610644+smoluwu@users.noreply.github.com>
2022-11-06 20:55:24 +0000

    cleaned up code

---
+++ src/main/java/caittastic/torchblocktastic/ModTorchBlock.java
@@ -0,0 +1,58 @@
+package caittastic.torchblocktastic;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+
+import static caittastic.torchblocktastic.TorchBlocktastic.TORCH_BLOCK_MAP;
+
+public class ModTorchBlock extends Block {
+    public ModTorchBlock(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+    @Override // makes our block flamable
+    public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 60;
+    }
+
+    @Override //gets the speed at wich our block spreads fire
+    public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 30;
+    }
+
+    @Override //result of right clicking on the block
+    public @NotNull InteractionResult use(BlockState blockState, Level level, BlockPos bPos, Player player, InteractionHand iHand, BlockHitResult bHitResult) {
+        InteractionHand mainHand = InteractionHand.MAIN_HAND; //main hand
+        ItemStack itemInMainHand = player.getItemInHand(mainHand); //item in main hand
+
+        BlockState whiteTorchBlockBlockState = TORCH_BLOCK_MAP.get("white").get().defaultBlockState(); //the default block state of the white torch block
+        //is the interaction hand the main hand and is the item in main hand a potion and is self a white torch block and is the held potion in main hand water
+        if(iHand == mainHand && itemInMainHand.is(Items.POTION) && PotionUtils.getPotion(itemInMainHand) == Potions.WATER && blockState != whiteTorchBlockBlockState){
+            //play cleaning noise
+            level.playSound(player, bPos, SoundEvents.BOTTLE_EMPTY, SoundSource.BLOCKS, 0.5F, level.getRandom().nextFloat() * 0.4F + 0.8F);
+            //set block to white torch block
+            level.setBlock(bPos, whiteTorchBlockBlockState, 3);
+            //if client side, return successful interaction
+            if(level.isClientSide()){
+                return InteractionResult.sidedSuccess(true);
+            }
+            return InteractionResult.sidedSuccess(false);
+        }
+        return super.use(blockState, level, bPos, player, iHand, bHitResult);
+    }
+}
--- src/main/java/caittastic/torchblocktastic/TorchBlocktastic.java
+++ src/main/java/caittastic/torchblocktastic/TorchBlocktastic.java
@@ -1,65 +1,65 @@
 package caittastic.torchblocktastic;
 
-import caittastic.torchblocktastic.init.BlockInit;
-import caittastic.torchblocktastic.init.ItemInit;
-import io.netty.util.Constant;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TorchBlocktastic.MOD_ID)
 public class TorchBlocktastic {
+    /*----------------------------------------------- variables -----------------------------------------------*/
+    public static final String MOD_ID = "torchblocktastic"; //a string that contains our modid, accessable anywhere
+    public static final String[] COLOURS = {"black", "brown", "blue", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "red", "white", "yellow"}; //all the colours our mod works with
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID); //block registry
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID); //item registry
+    public static final Map<String, RegistryObject<Block>> TORCH_BLOCK_MAP = new HashMap<>(); //map that contains torch blocks mapped to colours
+    /*----------------------------------------------- mod setup and running -----------------------------------------------*/
+    public TorchBlocktastic() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus(); //the bus for registries
+        BLOCKS.register(bus); //register block registry
+        ITEMS.register(bus); //register item registry
+        MinecraftForge.EVENT_BUS.register(this); // Register ourselves for server and other game events we are interested in
+    }
 
-    //all the colours our mod works with
-    public static final String[] colours = {"black", "brown", "blue", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "red", "white", "yellow"};
-
-    //a string that contains our modid, accessable anywhere
-    public static final String MOD_ID = "torchblocktastic";
-
-    //a creative tab for the stuff in our mod
-    public static final CreativeModeTab TORCH_BLOCK = new CreativeModeTab(MOD_ID) {
+    /*----------------------------------------------- tab -----------------------------------------------*/
+    public static final CreativeModeTab TORCH_BLOCK_TAB = new CreativeModeTab(MOD_ID) {
         @Override
         @OnlyIn(Dist.CLIENT)
         public ItemStack makeIcon() {
-            return new ItemStack(BlockInit.TORCH_BLOCK_BY_COLOUR.get("white").get());
+            return new ItemStack(TORCH_BLOCK_MAP.get("white").get());
         }
     };
 
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    public TorchBlocktastic() {
-        //registers items
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ItemInit.ITEMS.register(bus);
-        BlockInit.BLOCKS.register(bus);
-
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+    /*----------------------------------------------- mod block and item registering -----------------------------------------------*/
+    //for every colour, create a new torch block and add it to the hashmap mapped to the colour
+    static {
+        for (String colour : COLOURS) {
+            TORCH_BLOCK_MAP.put(colour, registerBlockWithBlockItem(colour + "_torch_block", () -> new ModTorchBlock(BlockBehaviour.Properties.copy(Blocks.WHITE_WOOL).strength(0.3f).lightLevel((p_50870_) -> 14)), TORCH_BLOCK_TAB));
+        }
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    //registers a block and block item
+    public static <T extends Block> RegistryObject<T> registerBlockWithBlockItem(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        ITEMS.register(name, () -> new BlockItem(toReturn.get(),
+                new Item.Properties().tab(tab)));
+        return toReturn;
     }
-
-
 }
